2fba5ced077b55e8c726b6dc7a6dd650574062a1
Siva Karthikeyan Krishnan <Sivak@pricoltech.com>
2017-12-27 07:28:29 +0530

    Add boilerplate sample for python 2.6 version

---
+++ lpthw/bp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module Docstring
+"""
+
+__author__ = "Siva Karthikeyan"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print "hello world"
+
+
+if __name__ == "__main__":
+    #""" This is executed when run from the command line """
+    main()
