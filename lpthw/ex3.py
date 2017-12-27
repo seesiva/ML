c5115f608ddfac0e75d30a9405856f0bbc54f1ed
Siva Karthikeyan Krishnan <Sivak@pricoltech.com>
2017-12-27 07:47:31 +0530

    updated the lint warnings

--- lpthw/ex3.py
+++ lpthw/ex3.py
@@ -1,9 +1,12 @@
+"""
+PEMDAS Math operators
+"""
 print "I will count my veggies"
 print "PEMDAS, which stands for Parentheses Exponents Multiplication Division Addition Subtraction."
 print "Carrots", 10+20
-print "Brinjals",100
-print "Modulus of 20/3 is: ", 20%3 #Modulus - Gives reminder 
-print "19/2 is: ",19/2
-print "19.0/2.0 is: ",19.0/2.0
-print "(1+1)*2/2+5-1 = ",(1+1)*2/2+5-1
+print "Brinjals", 100
+print "Modulus of 20/3 is: ", 20%3 #Modulus - Gives reminder
+print "19/2 is: ", 19/2
+print "19.0/2.0 is: ", 19.0/2.0
+print "(1+1)*2/2+5-1 = ", (1+1)*2/2+5-1
 print 1+2+3+4+5-4%2
