2dd6b956165ea3d42f9f95e6996d10dba1413501
Siva Karthikeyan Krishnan <Sivak@pricoltech.com>
2017-12-27 06:43:05 +0530

    Initial commit

---
+++ lpthw/ex1.py
@@ -0,0 +1,4 @@
+print "Hello World!"
+print 'This is amazing python stuff'
+print "I'll be happy to see if this is getting printed."
+print 'Double quote like "this" is amazing to get printed with python!!'
