3852039c71e7e4bd8975738a56f1510cb5cd25f1
Siva Karthikeyan Krishnan <Sivak@pricoltech.com>
2017-12-27 07:24:01 +0530

    Additional changes

--- lpthw/ex1.py
+++ lpthw/ex1.py
@@ -1,4 +1,8 @@
+"""
+Sample excerice
+"""
 print "Hello World!"
 print 'This is amazing python stuff'
 print "I'll be happy to see if this is getting printed."
 print 'Double quote like "this" is amazing to get printed with python!!'
+print "Potential error in the code."
