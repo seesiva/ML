6316bdeb5b039c4b6a112d581a506d413fff4c96
Siva Karthikeyan Krishnan <Sivak@pricoltech.com>
2017-12-28 14:06:00 +0530

    KNN Initial Notes

---
+++ lpthw/ex4.py
@@ -0,0 +1,11 @@
+"""
+Excerice 4 handling variables
+"""
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passangers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+car_pool_capacity = cars_driven * space_in_a_car
+avg_passangers_per_car=passangers/cars_driven
