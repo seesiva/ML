d426cb95d6b0465346110b41df845cdb3f15d5f3
Siva Karthikeyan <seesiva@gmail.com>
2018-01-24 05:01:39 +0530

    Updates to pca

--- mlprogs/pca/pca.py
+++ mlprogs/pca/pca.py
@@ -2,14 +2,19 @@
 PCA - Principal Component Analysis
 """
 import pandas as pd
-from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
 
 
 def main():
-    data = pd.read_csv("sampledata.csv",skiprows=1, header=None)
-    data.columns = ["V"+str(i) for i in range(1, len(data.columns)+1)]  # rename column names to be similar to R naming convention
-    data.V1 = data.V1.astype(str)
-    pd.tools.plotting.scatter_matrix(data.loc, diagonal="kde")
-    plt.tight_layout()
-    plt.show()
-    
+	print "Principal Component Analysis"
+	df = pd.read_csv("sampledata.csv",skiprows=1, header=None)
+    	print "Completed reading data.."
+	df.columns = ["V"+str(i) for i in range(1, len(df.columns)+1)] 
+	print df.tail()
+	pca = PCA(n_components=10)
+    	pca.fit(df)
+    	print(pca.explained_variance_ratio_)
+
+if __name__ == '__main__':
+	main()
