8b0c1fbe5cfb0854b1956412c497a154824705e9
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-02-12 00:23:27 +0530

    Updated code to reflect the respective titles in the output

--- mlprogs/fivenumsummary.py
+++ mlprogs/fivenumsummary.py
@@ -4,10 +4,11 @@ Five number summary example
 import numpy as np
 # Students who has scored in Tamil Language
 tamilMarks = np.array([82,74,56,35,89,78,90,100,84,65,54,59,45])
+summaryTitles = ["Min","Lower Quartile", "Median", "Upper Quartile", "Maximum"]
 tukey = [np.min(tamilMarks),
           np.percentile(tamilMarks, 25, interpolation='midpoint'),
           np.median(tamilMarks),
           np.percentile(tamilMarks, 75, interpolation='midpoint'),
           np.max(tamilMarks)]
-for v in tukey:
-     print(v)
+for index, v in enumerate(tukey):
+    print(summaryTitles[index], ':',v)
