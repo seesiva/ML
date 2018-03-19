40bc7a6ec1a2e0669c20570c05456c9d9595d690
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 15:35:10 +0530

    Sys library of python

---
+++ lpthw/ex6.py
@@ -0,0 +1,9 @@
+"""
+Get to the python variables from program using of str function
+"""
+import sys
+
+print "Python Version: " + str(sys.version)
+print "Python API Version: " + str(sys.api_version)
+print "Python API Version Info: " + str(sys.version_info)
+print sys.copyright
