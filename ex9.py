bbb2c537797478c1cb416e5076dcdbd2ae0936eb
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 17:15:48 +0530

    Function as a return value

---
+++ ex9.py
@@ -0,0 +1,27 @@
+"""
+HoF:Function as a return value
+"""
+def add_two_nums(x,y):
+    return x+y
+
+def add_three_nums(x,y,z):
+    return x+y+z
+
+def get_appropriate_function(num_len):
+    if num_len===3:
+        return add_three_nums
+    else:
+        return add_two_nums
+
+if __name__=="__main__":
+    args = [1,2,3]
+    num_len=len(args)
+    res_function=get_appropriate_function(num_len)
+    print(res_function)
+    print (res_function(args))
+
+    args = [1,2]
+    num_len=len(args)
+    res_function=get_appropriate_function(num_len)
+    print(res_function) # when length is 2
+    print (res_function(args)) # Addition according 2 parameters
