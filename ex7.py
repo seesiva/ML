ae12589b01ce614fd923cf98e89427296b793e97
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 17:02:31 +0530

    Higher Order Functions-Handling function as arguments

---
+++ ex7.py
@@ -0,0 +1,14 @@
+"""
+Python program to display todays date and time
+"""
+import datetime
+now = datetime.datetime.now()
+print now
+print (now.strftime("%Y-%m-%d %H:%M:%S")) 
+""" 
+Read the datetime tuple
+"""
+dt = datetime.datetime.now()
+tt = dt.timetuple()
+for it in tt:   
+    print it
---
+++ ex8.py
@@ -0,0 +1,13 @@
+"""
+Higher Order Functions:
+HoF: Handling function as arugments
+"""
+def summation(nums):
+    return sum(nums)
+
+def main(f, *args):
+    result = f(*args)
+    print (result)
+
+if __name__=="__main__":
+    main(summation, [1,2,3,4]) # output 6
