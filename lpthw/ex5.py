b973dfce3a1f41446ac3eb136f896d68a2235291
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 15:06:53 +0530

    Added a small program to update

---
+++ lpthw/ex5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Get the Input and print it back
+"""
+def main():
+    name = raw_input ("Please enter your name: ")
+    print "Hello, "+name
+
+if __name__ == "__main__":
+    #""" This is executed when run from the command line """
+    main()
