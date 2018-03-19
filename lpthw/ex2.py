cd5d85db26e3c09342ad052e074999e5458441c3
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 15:14:14 +0530

    Use of newline character sample excerise

---
+++ lpthw/ex2.py
@@ -0,0 +1,10 @@
+"""
+Print twinkle twinkle little star in specific format
+Use of newline character with in the string
+"""
+def main():
+    rhyme="Twinkle, twinkle, little star,\n How I wonder what you are!\n Up above the world so high,\n Like a diamond in the sky.\n Twinkle, twinkle, little star,\n How I wonder what you are\n"
+    print rhyme
+
+if __name__=="__main__":
+    main()
