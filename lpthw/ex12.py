8f3f52910f7752ac17994e400d161a6a8adba01b
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-03-19 17:47:16 +0530

    Program to handle dictionary and raw_input

---
+++ lpthw/ex12.py
@@ -0,0 +1,16 @@
+"""
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) 
+such that is an integral number between 1 and n (both included). and 
+then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+"""
+
+n=int(raw_input("Enter the input integer")) # get the raw input as integer
+d=dict() #Use of dictionary
+for i in range(1,n):
+    d[i]=i*i
+
+print d
