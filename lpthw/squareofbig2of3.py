eb7d164dd5c99af875a4adb98aac321a23e5b6cc
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-06-23 07:14:50 +0530

    Added unit tests

--- lpthw/squareofbig2of3.py
+++ lpthw/squareofbig2of3.py
@@ -1,6 +1,7 @@
 """
 Develop a procedure which will return the sum of square of largest of 2 numbers from given 3 numbers
 """
+import unittest
 
 def square(x):
     return x * x
@@ -19,7 +20,19 @@ def sumofsquarebig2of3(x,y,z):
     return sumofsquare(big1,big2)
 
 
+class MyTests(unittest.TestCase):
+    def test_Square(self):
+        self.assertEqual(square(2),4)
+    
+    def test_SumofSquare(self):
+        self.assertEqual(sumofsquare(2,2),8)
+    
+    def test_SumofSquareNegative(self):
+        self.assertEqual(sumofsquare(-1,-1),2)
+
+    def test_SumofSquareof2ofBigof3(self):
+        self.assertEqual(sumofsquarebig2of3(1,2,3),13)
+
 if __name__=="__main__":
+    unittest.main()
     print sumofsquarebig2of3(1,2,3)
-
-
